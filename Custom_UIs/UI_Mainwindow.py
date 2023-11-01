ee32c403080defbd281dd31159a618fdfec882dd
garid3000 <garidzorigoo@gmail.com>
2023-11-01 15:48:44 +0900

    UI: new layout, for better User Experience when operating File-Explorer

--- Custom_UIs/UI_Mainwindow.py
+++ Custom_UIs/UI_Mainwindow.py
@@ -48,7 +48,7 @@ class Ui_MainWindow(object):
     def setupUi(self, MainWindow: QMainWindow) -> None:
         if not MainWindow.objectName():
             MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(1280, 720)
+        MainWindow.resize(856, 597)
         self.actionOpen_Directory = QAction(MainWindow)
         self.actionOpen_Directory.setObjectName("actionOpen_Directory")
         self.actionSave_Raw_Bayer_as = QAction(MainWindow)
@@ -73,117 +73,22 @@ class Ui_MainWindow(object):
         self.centralwidget.setObjectName("centralwidget")
         self.gridLayout = QGridLayout(self.centralwidget)
         self.gridLayout.setObjectName("gridLayout")
-        self.groupBox_3 = QGroupBox(self.centralwidget)
-        self.groupBox_3.setObjectName("groupBox_3")
-        self.gridLayout_5 = QGridLayout(self.groupBox_3)
-        self.gridLayout_5.setObjectName("gridLayout_5")
-        self.splitter = QSplitter(self.groupBox_3)
+        self.splitter = QSplitter(self.centralwidget)
         self.splitter.setObjectName("splitter")
-        self.splitter.setLineWidth(2)
         self.splitter.setOrientation(Qt.Orientation.Horizontal)
-        self.gp_webcam_meta = QGroupBox(self.splitter)
-        self.gp_webcam_meta.setObjectName("gp_webcam_meta")
-        self.verticalLayout = QVBoxLayout(self.gp_webcam_meta)
-        self.verticalLayout.setObjectName("verticalLayout")
-        self.limg_webcam = QLabelClick(self.gp_webcam_meta)
-        self.limg_webcam.setObjectName("limg_webcam")
-
-        self.verticalLayout.addWidget(self.limg_webcam)
-
-        self.tb_meta_json = QTextBrowser(self.gp_webcam_meta)
-        self.tb_meta_json.setObjectName("tb_meta_json")
-        font = QFont()
-        font.setFamilies(["Monospace"])
-        self.tb_meta_json.setFont(font)
-
-        self.verticalLayout.addWidget(self.tb_meta_json)
-
-        self.verticalSpacer_2 = QSpacerItem(
-            20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding
-        )
-
-        self.verticalLayout.addItem(self.verticalSpacer_2)
-
-        self.verticalLayout.setStretch(0, 2)
-        self.verticalLayout.setStretch(1, 1)
-        self.splitter.addWidget(self.gp_webcam_meta)
-        self.gp_spectral_panel = QGroupBox(self.splitter)
-        self.gp_spectral_panel.setObjectName("gp_spectral_panel")
-        self.gridLayout_4 = QGridLayout(self.gp_spectral_panel)
-        self.gridLayout_4.setObjectName("gridLayout_4")
-        self.pb_export = QPushButton(self.gp_spectral_panel)
-        self.pb_export.setObjectName("pb_export")
-
-        self.gridLayout_4.addWidget(self.pb_export, 1, 2, 1, 1)
-
-        self.pb_conf_export = QPushButton(self.gp_spectral_panel)
-        self.pb_conf_export.setObjectName("pb_conf_export")
-
-        self.gridLayout_4.addWidget(self.pb_conf_export, 1, 1, 1, 1)
-
-        self.horizontalSpacer = QSpacerItem(
-            40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum
-        )
-
-        self.gridLayout_4.addItem(self.horizontalSpacer, 1, 0, 1, 1)
-
-        self.tabWidget = QTabWidget(self.gp_spectral_panel)
-        self.tabWidget.setObjectName("tabWidget")
-        self.tab_1 = QWidget()
-        self.tab_1.setObjectName("tab_1")
-        self.gridLayout_6 = QGridLayout(self.tab_1)
-        self.gridLayout_6.setObjectName("gridLayout_6")
-        self.limg_bayer_full = QLabelClick(self.tab_1)
-        self.limg_bayer_full.setObjectName("limg_bayer_full")
-
-        self.gridLayout_6.addWidget(self.limg_bayer_full, 0, 0, 1, 1)
-
-        self.limg_bayer_gray = QLabelClick(self.tab_1)
-        self.limg_bayer_gray.setObjectName("limg_bayer_gray")
-
-        self.gridLayout_6.addWidget(self.limg_bayer_gray, 1, 0, 1, 1)
-
-        self.limg_bayer_obje = QLabelClick(self.tab_1)
-        self.limg_bayer_obje.setObjectName("limg_bayer_obje")
-
-        self.gridLayout_6.addWidget(self.limg_bayer_obje, 2, 0, 1, 1)
-
-        self.tabWidget.addTab(self.tab_1, "")
-        self.tab_2 = QWidget()
-        self.tab_2.setObjectName("tab_2")
-        self.verticalLayout_4 = QVBoxLayout(self.tab_2)
-        self.verticalLayout_4.setObjectName("verticalLayout_4")
-        self.limg_raw_spectrum = QLabelClick(self.tab_2)
-        self.limg_raw_spectrum.setObjectName("limg_raw_spectrum")
-
-        self.verticalLayout_4.addWidget(self.limg_raw_spectrum)
-
-        self.tabWidget.addTab(self.tab_2, "")
-        self.tab_3 = QWidget()
-        self.tab_3.setObjectName("tab_3")
-        self.verticalLayout_5 = QVBoxLayout(self.tab_3)
-        self.verticalLayout_5.setObjectName("verticalLayout_5")
-        self.limg_refl_spectrum = QLabelClick(self.tab_3)
-        self.limg_refl_spectrum.setObjectName("limg_refl_spectrum")
-
-        self.verticalLayout_5.addWidget(self.limg_refl_spectrum)
-
-        self.tabWidget.addTab(self.tab_3, "")
-
-        self.gridLayout_4.addWidget(self.tabWidget, 0, 0, 1, 3)
-
-        self.splitter.addWidget(self.gp_spectral_panel)
-
-        self.gridLayout_5.addWidget(self.splitter, 0, 0, 1, 1)
-
-        self.gridLayout.addWidget(self.groupBox_3, 0, 1, 3, 1)
-
-        self.gb_dir_panel = QGroupBox(self.centralwidget)
+        self.widget = QWidget(self.splitter)
+        self.widget.setObjectName("widget")
+        self.verticalLayout_2 = QVBoxLayout(self.widget)
+        self.verticalLayout_2.setObjectName("verticalLayout_2")
+        self.verticalLayout_2.setContentsMargins(0, 0, 0, 0)
+        self.gb_dir_panel = QGroupBox(self.widget)
         self.gb_dir_panel.setObjectName("gb_dir_panel")
         self.gridLayout_3 = QGridLayout(self.gb_dir_panel)
         self.gridLayout_3.setObjectName("gridLayout_3")
         self.tv_dir = QTreeView(self.gb_dir_panel)
         self.tv_dir.setObjectName("tv_dir")
+        font = QFont()
+        font.setFamilies(["Monospace"])
         self.tv_dir.setFont(font)
 
         self.gridLayout_3.addWidget(self.tv_dir, 2, 0, 1, 1)
@@ -204,9 +109,9 @@ class Ui_MainWindow(object):
 
         self.gridLayout_3.addWidget(self.le_data_dir, 0, 0, 1, 1)
 
-        self.gridLayout.addWidget(self.gb_dir_panel, 0, 0, 1, 1)
+        self.verticalLayout_2.addWidget(self.gb_dir_panel)
 
-        self.gb_control_panel = QGroupBox(self.centralwidget)
+        self.gb_control_panel = QGroupBox(self.widget)
         self.gb_control_panel.setObjectName("gb_control_panel")
         self.gridLayout_2 = QGridLayout(self.gb_control_panel)
         self.gridLayout_2.setObjectName("gridLayout_2")
@@ -305,17 +210,109 @@ class Ui_MainWindow(object):
         self.gridLayout_2.setColumnStretch(1, 1)
         self.gridLayout_2.setColumnStretch(2, 1)
 
-        self.gridLayout.addWidget(self.gb_control_panel, 2, 0, 1, 1)
+        self.verticalLayout_2.addWidget(self.gb_control_panel)
 
-        self.gridLayout.setColumnStretch(0, 1)
-        self.gridLayout.setColumnStretch(1, 3)
+        self.splitter.addWidget(self.widget)
+        self.gp_webcam_meta = QGroupBox(self.splitter)
+        self.gp_webcam_meta.setObjectName("gp_webcam_meta")
+        self.verticalLayout = QVBoxLayout(self.gp_webcam_meta)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.limg_webcam = QLabelClick(self.gp_webcam_meta)
+        self.limg_webcam.setObjectName("limg_webcam")
+
+        self.verticalLayout.addWidget(self.limg_webcam)
+
+        self.tb_meta_json = QTextBrowser(self.gp_webcam_meta)
+        self.tb_meta_json.setObjectName("tb_meta_json")
+        self.tb_meta_json.setFont(font)
+
+        self.verticalLayout.addWidget(self.tb_meta_json)
+
+        self.verticalSpacer_2 = QSpacerItem(
+            20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding
+        )
+
+        self.verticalLayout.addItem(self.verticalSpacer_2)
+
+        self.verticalLayout.setStretch(0, 2)
+        self.verticalLayout.setStretch(1, 1)
+        self.splitter.addWidget(self.gp_webcam_meta)
+        self.gp_spectral_panel = QGroupBox(self.splitter)
+        self.gp_spectral_panel.setObjectName("gp_spectral_panel")
+        self.gridLayout_4 = QGridLayout(self.gp_spectral_panel)
+        self.gridLayout_4.setObjectName("gridLayout_4")
+        self.pb_export = QPushButton(self.gp_spectral_panel)
+        self.pb_export.setObjectName("pb_export")
+
+        self.gridLayout_4.addWidget(self.pb_export, 1, 2, 1, 1)
+
+        self.pb_conf_export = QPushButton(self.gp_spectral_panel)
+        self.pb_conf_export.setObjectName("pb_conf_export")
+
+        self.gridLayout_4.addWidget(self.pb_conf_export, 1, 1, 1, 1)
+
+        self.horizontalSpacer = QSpacerItem(
+            40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum
+        )
+
+        self.gridLayout_4.addItem(self.horizontalSpacer, 1, 0, 1, 1)
+
+        self.tabWidget = QTabWidget(self.gp_spectral_panel)
+        self.tabWidget.setObjectName("tabWidget")
+        self.tab_1 = QWidget()
+        self.tab_1.setObjectName("tab_1")
+        self.gridLayout_6 = QGridLayout(self.tab_1)
+        self.gridLayout_6.setObjectName("gridLayout_6")
+        self.limg_bayer_full = QLabelClick(self.tab_1)
+        self.limg_bayer_full.setObjectName("limg_bayer_full")
+
+        self.gridLayout_6.addWidget(self.limg_bayer_full, 0, 0, 1, 1)
+
+        self.limg_bayer_gray = QLabelClick(self.tab_1)
+        self.limg_bayer_gray.setObjectName("limg_bayer_gray")
+
+        self.gridLayout_6.addWidget(self.limg_bayer_gray, 1, 0, 1, 1)
+
+        self.limg_bayer_obje = QLabelClick(self.tab_1)
+        self.limg_bayer_obje.setObjectName("limg_bayer_obje")
+
+        self.gridLayout_6.addWidget(self.limg_bayer_obje, 2, 0, 1, 1)
+
+        self.tabWidget.addTab(self.tab_1, "")
+        self.tab_2 = QWidget()
+        self.tab_2.setObjectName("tab_2")
+        self.verticalLayout_4 = QVBoxLayout(self.tab_2)
+        self.verticalLayout_4.setObjectName("verticalLayout_4")
+        self.limg_raw_spectrum = QLabelClick(self.tab_2)
+        self.limg_raw_spectrum.setObjectName("limg_raw_spectrum")
+
+        self.verticalLayout_4.addWidget(self.limg_raw_spectrum)
+
+        self.tabWidget.addTab(self.tab_2, "")
+        self.tab_3 = QWidget()
+        self.tab_3.setObjectName("tab_3")
+        self.verticalLayout_5 = QVBoxLayout(self.tab_3)
+        self.verticalLayout_5.setObjectName("verticalLayout_5")
+        self.limg_refl_spectrum = QLabelClick(self.tab_3)
+        self.limg_refl_spectrum.setObjectName("limg_refl_spectrum")
+
+        self.verticalLayout_5.addWidget(self.limg_refl_spectrum)
+
+        self.tabWidget.addTab(self.tab_3, "")
+
+        self.gridLayout_4.addWidget(self.tabWidget, 0, 0, 1, 3)
+
+        self.splitter.addWidget(self.gp_spectral_panel)
+
+        self.gridLayout.addWidget(self.splitter, 0, 0, 1, 1)
+
         MainWindow.setCentralWidget(self.centralwidget)
         self.statusbar = QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
         MainWindow.setStatusBar(self.statusbar)
         self.menubar = QMenuBar(MainWindow)
         self.menubar.setObjectName("menubar")
-        self.menubar.setGeometry(QRect(0, 0, 1280, 19))
+        self.menubar.setGeometry(QRect(0, 0, 856, 19))
         self.menuFile = QMenu(self.menubar)
         self.menuFile.setObjectName("menuFile")
         self.menuTutorial = QMenu(self.menubar)
@@ -376,7 +373,33 @@ class Ui_MainWindow(object):
         self.actionContact.setText(
             QCoreApplication.translate("MainWindow", "Contact", None)
         )
-        self.groupBox_3.setTitle(QCoreApplication.translate("MainWindow", "Data", None))
+        self.gb_dir_panel.setTitle(
+            QCoreApplication.translate("MainWindow", "Directory Control Panel", None)
+        )
+        self.pb_data_dir.setText(
+            QCoreApplication.translate("MainWindow", "Set root directory ???", None)
+        )
+        self.gb_control_panel.setTitle(
+            QCoreApplication.translate("MainWindow", "Geometry control panel", None)
+        )
+        self.pb_refresh.setText(
+            QCoreApplication.translate("MainWindow", "Refresh (Redraw)", None)
+        )
+        self._label_1.setText(
+            QCoreApplication.translate("MainWindow", "Horizontal Left Pixel", None)
+        )
+        self._label_2.setText(
+            QCoreApplication.translate("MainWindow", "Vert. Gray Pixel Range", None)
+        )
+        self._label_4.setText(
+            QCoreApplication.translate("MainWindow", "Horizontal Fraunhofer", None)
+        )
+        self._label_3.setText(
+            QCoreApplication.translate("MainWindow", "Vert. Object Pixel Range", None)
+        )
+        self.cb_fraunhofer.setText(
+            QCoreApplication.translate("MainWindow", "Calibrate", None)
+        )
         self.gp_webcam_meta.setTitle(
             QCoreApplication.translate("MainWindow", "Webcam + Meta Data", None)
         )
@@ -414,33 +437,6 @@ class Ui_MainWindow(object):
         self.tabWidget.setTabText(
             self.tabWidget.indexOf(self.tab_3),
             QCoreApplication.translate("MainWindow", "Spectrum (Reflectance)", None),
-        )
-        self.gb_dir_panel.setTitle(
-            QCoreApplication.translate("MainWindow", "Directory Control Panel", None)
-        )
-        self.pb_data_dir.setText(
-            QCoreApplication.translate("MainWindow", "Set root directory ???", None)
-        )
-        self.gb_control_panel.setTitle(
-            QCoreApplication.translate("MainWindow", "Geometry control panel", None)
-        )
-        self.pb_refresh.setText(
-            QCoreApplication.translate("MainWindow", "Refresh (Redraw)", None)
-        )
-        self._label_1.setText(
-            QCoreApplication.translate("MainWindow", "Horizontal Left Pixel", None)
-        )
-        self._label_2.setText(
-            QCoreApplication.translate("MainWindow", "Vert. Gray Pixel Range", None)
-        )
-        self._label_4.setText(
-            QCoreApplication.translate("MainWindow", "Horizontal Fraunhofer", None)
-        )
-        self._label_3.setText(
-            QCoreApplication.translate("MainWindow", "Vert. Object Pixel Range", None)
-        )
-        self.cb_fraunhofer.setText(
-            QCoreApplication.translate("MainWindow", "Calibrate", None)
         )
         self.menuFile.setTitle(QCoreApplication.translate("MainWindow", "File", None))
         self.menuTutorial.setTitle(
