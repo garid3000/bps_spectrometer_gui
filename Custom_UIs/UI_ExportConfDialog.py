c781a1180e6877a08040e17f7e2dc471732837ac
garid3000 <garidzorigoo@gmail.com>
2023-11-01 22:04:31 +0900

    pyside uic tool output from yay (not from pip)

--- Custom_UIs/UI_ExportConfDialog.py
+++ Custom_UIs/UI_ExportConfDialog.py
@@ -3,24 +3,55 @@
 ################################################################################
 ## Form generated from reading UI file 'UI_ExportConfDialog.ui'
 ##
-## Created by: Qt User Interface Compiler version 6.5.2
+## Created by: Qt User Interface Compiler version 6.6.0
 ##
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
 
 from PySide6.QtCore import (
     QCoreApplication,
+    QDate,
+    QDateTime,
+    QLocale,
     QMetaObject,
+    QObject,
+    QPoint,
+    QRect,
+    QSize,
+    QTime,
+    QUrl,
     Qt,
 )
+from PySide6.QtGui import (
+    QBrush,
+    QColor,
+    QConicalGradient,
+    QCursor,
+    QFont,
+    QFontDatabase,
+    QGradient,
+    QIcon,
+    QImage,
+    QKeySequence,
+    QLinearGradient,
+    QPainter,
+    QPalette,
+    QPixmap,
+    QRadialGradient,
+    QTransform,
+)
 from PySide6.QtWidgets import (
+    QAbstractButton,
+    QApplication,
     QCheckBox,
+    QDialog,
     QDialogButtonBox,
     QGridLayout,
     QGroupBox,
     QSizePolicy,
     QSpacerItem,
     QVBoxLayout,
+    QWidget,
 )
 
 
